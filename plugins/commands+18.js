93771fa4566fd8a8349c8632c8649843ed3f9e97
Proximist <103359694+Proximist@users.noreply.github.com>
2023-02-20 23:24:05 +0530

    Update commands+18.js

--- plugins/commands+18.js
+++ plugins/commands+18.js
@@ -2,8 +2,6 @@ import axios from "axios"
 import fetch from 'node-fetch'
 let handler = async (m, {command, usedPrefix, conn}) => {
 if (!global.db.data.chats[m.chat].nsfw) throw `🚫 group doesnt supprt nsfw \n\n enable it by \n*${usedPrefix}enable* nsfw`
-    let user = global.db.data.users[m.sender].age
-    if (user < 17) throw m.reply(`❎ uneed to be atleast 18 years`)
 
 if (command == 'hloli') {
 let res = (await axios.get(`https://raw.githubusercontent.com/Guru322/api/Guru/BOT-JSON/nsfwloli.json`)).data  
